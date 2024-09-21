8d8cbbd8cdefeb615475c6cd5a0296dfa5355086
Robin Appelman <robin@icewind.nl>
2024-09-21 14:16:04 +0200

    ci

--- flake.nix
+++ flake.nix
@@ -8,8 +8,14 @@
     self,
     nixpkgs,
     utils,
-  }:
-    utils.lib.eachDefaultSystem (system: let
+  }: let
+      platformVersions = ["0D1" "0D2" "0D3" "0D4" "0D5" "0D6" "0D7" "1D0" "1D1" "1D2" "1D3" "1D4"];
+      bitstreams = ["analyzer" "selftest" "facedancer"];
+      gateware-matrix = nixpkgs.lib.mapCartesianProduct nixpkgs.lib.trivial.id {
+        platform = platformVersions;
+        bitstream = bitstreams;
+      };
+    in utils.lib.eachDefaultSystem (system: let
       overlays = [
         (import ./overlay.nix)
       ];
@@ -17,15 +23,31 @@
         inherit system overlays;
       };
       inherit (pkgs) lib;
+
+      cynthion-gateware-individual = lib.listToAttrs (map ({
+          platform,
+          bitstream,
+        }: {
+          name = "${bitstream}-${platform}";
+          value = pkgs.cynthion-gateware-single.override {
+            inherit bitstream;
+            platform = "CynthionPlatformRev${platform}";
+          };
+        })
+        gateware-matrix);
     in rec {
       devShells.default =
         pkgs.mkShell {
         };
       packages = {
         inherit (pkgs) packetry cynthion cynthion-udev apollo-cynthion cynthion-gateware apollo-fpga;
+        inherit cynthion-gateware-individual;
       };
     })
     // {
       overlays.default = import ./overlay.nix;
+      gateware-matrix = {
+        include = gateware-matrix;
+      };
     };
 }
