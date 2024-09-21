f66496d454a03254962e942ccb4a83fb8f8cf0ae
Robin Appelman <robin@icewind.nl>
2024-09-21 13:55:55 +0200

    make gateware bitstreams and platform versions configurable

--- pkgs/cynthion/gateware.nix
+++ pkgs/cynthion/gateware.nix
@@ -3,11 +3,11 @@
   cynthion-gateware-single,
   cynthion-unwrapped,
   lib,
+  platformVersions ? ["0D1" "0D2" "0D3" "0D4" "0D5" "0D6" "0D7" "1D0" "1D1" "1D2" "1D3" "1D4"],
+  bitstreams ? ["analyzer" "selftest" "facedancer"],
   ...
 }: let
   inherit (lib) mapCartesianProduct;
-  platformVersions = ["0D1" "0D2" "0D3" "0D4" "0D5" "0D6" "0D7" "1D0" "1D1" "1D2" "1D3" "1D4"];
-  bitstreams = ["analyzer" "selftest" "facedancer"];
   gatewares =
     mapCartesianProduct ({
       platform,
